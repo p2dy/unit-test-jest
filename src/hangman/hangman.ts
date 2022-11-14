802573aff0ed11610de121518d9ff4339eac08fc
Patrick Friedrich <patrick.friedrich@andrena.de>
2022-11-14 11:45:06 +0100

    finish implementation

--- src/hangman/hangman.ts
+++ src/hangman/hangman.ts
@@ -1,17 +1,29 @@
 export class Hangman {
-    private word: string;
+    private readonly word: string;
+    private readonly hiddenWord: string;
 
     public constructor(word: string) {
         this.word = word;
+        this.hiddenWord = '*'.repeat(this.word.length)
     }
 
-    public guess(letter: string): boolean {
+    public guess(letter: string): Guess {
         if (letter.length != 1) {
             throw new Error('A guess should contain only a single letter!')
         }
-        return this.word.includes(letter);
-    }
 
+        let resultWord = this.hiddenWord;
+        for (let position = 0; position < this.word.length; position++) {
+            if (this.word[position] === letter) {
+                resultWord = resultWord.substring(0, position) + letter + resultWord.substring(position + letter.length)
+            }
+        }
+
+        return {
+            isCorrect: this.word.includes(letter),
+            resultWord: resultWord
+        };
+    }
 }
 
 export interface Guess {
